5f33387352a17d1b67ce592d691f7117d8bc5ca4
Gian Lazzarini <gianlazzarini@gmail.com>
2017-10-28 15:24:17 -0700

    Initial Commit with C# Console App Project Files

---
+++ PDF_parse_db/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PDF_parse_db
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
